f1ef3ffe7856f134c654817ffbd9a95ca1cf2826
Jesse Nicolas <jn@nevada.unr.edu>
2022-09-09 19:25:59 -0700

    food initializes to random position

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let snakePosition 
-let foodPosition
+let snakePosition = {}
+let foodPosition = {}
 //array that will hold the location of each snake div as the game rolls forward
 let snakeArr = []
 let seconds = 0
@@ -32,34 +32,40 @@ function handleClick(evt) {
 
   if (evt.code === `ArrowUp`) {
     moveSnake('up')
-    console.log('up arrow!')
   }
   else if (evt.code === `ArrowDown`) {
     moveSnake('down')
-    console.log("down arrow!")
   }
   else if (evt.code === `ArrowRight`) {
     moveSnake('right')
-    console.log("right arrow!")
   }
   else if (evt.code === `ArrowLeft`) {
     moveSnake('left')
-    console.log("left arrow!")
   }
   else if (evt.code === `Space`) {
     // this will reset the game after you lose
     direction = ''
-    console.log("space bar!")
   }
 }
 
-
+init()
 
 function init() {
+  snek.style.gridRowStart = 10
+  snek.style.gridColumnStart = 10
+  renderFood()
   // timerInterval = setInterval(moveSnake(), 1000)
-
   // console.log(timerInterval)  
 }
+
+function renderFood()  {
+  food.style.gridColumnStart = (Math.floor(Math.random() * 20))
+  food.style.gridRowStart = (Math.floor(Math.random() * 20))
+  foodPosition.x = food.style.gridColumnStart
+  foodPosition.y = food.style.gridRowStart
+  console.log(foodPosition)
+}
+
 
 function moveSnake(direction)  {
   if (direction === 'down') {
@@ -74,6 +80,9 @@ function moveSnake(direction)  {
   else if (direction === 'left')  {
     snek.style.gridColumnStart--
   }
+  snakePosition.x = snek.style.gridColumnStart
+  snakePosition.y = snek.style.gridRowStart
+  console.log(snakePosition)
 
 
 }
